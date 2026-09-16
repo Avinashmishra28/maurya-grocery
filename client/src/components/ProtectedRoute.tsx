211ff36d56fdd7f17c5c7049dc28c82878529c15
avinash <avinashmishracode23@gmail.com>
2026-09-16 13:13:13 +0530

    feat: add components functionality

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate, Outlet } from "react-router-dom"
+import { useAuth } from "../context/useAuth"
+import Loading from "./Loading"
+
+
+const ProtectedRoute = () => {
+  const {user, loading} = useAuth()
+
+  if(loading) return <Loading/>
+  if(!user) return <Navigate to='/login' replace/>
+  return (
+    <div>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default ProtectedRoute
