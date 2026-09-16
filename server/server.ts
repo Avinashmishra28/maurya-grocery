41f93a6d584825e6349211f5f6fc2f1b0478d01c
avinash <avinashmishracode23@gmail.com>
2026-09-16 16:47:53 +0530

    add stripe webhook route

--- server/server.ts
+++ server/server.ts
@@ -12,15 +12,19 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import adminRouter from "./routes/adminRoutes.js";
 import deliveryPartnerRoute from "./routes/deliveyPartnerRoutes.js";
+import { stripeWebhook } from "./controllers/webhooksController.js";
 
 
 const app = express();
+
+app.post("/api/stripe", express.raw({type: `application/json`}),
+stripeWebhook);
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-const port = Number(process.env.PORT) || 5000;
+const port = Number(process.env.PORT) || 5000; 
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Server is live!");
@@ -42,5 +46,5 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running on port ${port}`);
 });
